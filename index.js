5c20d81d4e72731cef7c3803dde15903ac132c0b
Lulytass <88911233+Lulytass@users.noreply.github.com>
2021-11-02 15:26:27 -0300

    primer modificacion

---
+++ index.js
@@ -0,0 +1,12 @@
+//              MODAL USUARIO
+const abrir = document.querySelector("#contacto")
+
+function abreModal(){
+    abrir.style.display = "flex"
+}
+function cierraModal(){
+    abrir.style.display = "none"
+}
+//            CARRITO DE COMPRAS
+
+//                   CHAT
---
+++ mujer/mujer.js
@@ -0,0 +1,64 @@
+//              MODAL USUARIO
+const abrir = document.querySelector("#contacto")
+
+function abreModal(){
+    abrir.style.display = "flex"
+}
+function cierraModal(){
+    abrir.style.display = "none"
+}
+//            CARRITO DE COMPRAS
+
+//                   CHAT
+
+//                  FILTROS
+let malla1 = document.querySelectorAll(".malla");
+let arriba1 = document.querySelectorAll(".arriba");
+let conjunto1 = document.querySelectorAll(".conjuntos");
+let abajo1 = document.querySelectorAll(".abajo");
+
+function abajo(){
+    for(var i = 0; i< malla1.length; i++)
+    malla1[i].classList.toggle("chau");
+    for(var i = 0; i< arriba1.length; i++)
+    arriba1[i].classList.toggle("chau");
+    for(var i = 0; i< conjunto1.length; i++)
+    conjunto1[i].classList.toggle("chau");
+}
+function arriba(){
+    for(var i = 0; i< abajo1.length; i++)
+    abajo1[i].classList.toggle("chau");
+    for(var i = 0; i< malla1.length; i++)
+    malla1[i].classList.toggle("chau");
+    for(var i = 0; i< conjunto1.length; i++)
+    conjunto1[i].classList.toggle("chau");
+}
+function mallas(){
+    for(var i = 0; i< abajo1.length; i++)
+    abajo1[i].classList.toggle("chau");
+    for(var i = 0; i< arriba1.length; i++)
+    arriba1[i].classList.toggle("chau");
+    for(var i = 0; i< arriba1.length; i++)
+    conjunto1[i].classList.toggle("chau");
+}
+function conjunto(){
+    for(var i = 0; i< abajo1.length; i++)
+    abajo1[i].classList.toggle("chau");
+    for(var i = 0; i< arriba1.length; i++)
+    arriba1[i].classList.toggle("chau");
+    for(var i = 0; i< malla1.length; i++)
+    malla1[i].classList.toggle("chau");
+}
+                //BOTON FILTRO
+let botonFiltro = document.querySelector(".botonFiltro");
+let filtros = document.querySelector(".losFiltros");
+botonFiltro.addEventListener("click", function(){
+   if(filtros.classList.contains("losFiltros")){
+       filtros.classList.remove("losFiltros")
+       filtros.classList.add("seVe")
+   }else{
+        filtros.classList.add("losFiltros")
+   }
+})
+
+
